8a7d3d642eb28da40cd45f81e9d727011e4873e1
Luciano7 <lucky.h.fatch@gmail.com>
2024-08-02 20:14:57 +0000

    safely get value

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
 """
-module: 100-safe_first_element
-python ducktyping anotation
+This module contains the safe_first_element function that retrieves
+the first element of a sequence if it exists, or None if it doesn't.
 """
 
 from typing import Sequence, Any, Union
 
-
-def safe_first_element(lst: Sequence) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
-    Returns the first element of a sequence
-    if it exists, otherwise returns None.
+    Safely get the first element of a sequence.
 
-    Args:
-        lst (Sequence): A sequence of elements.
+    Parameters:
+    - lst (Sequence[Any]): The sequence to get the first element from.
 
     Returns:
-        Union[Any, None]: The first element of the
-        sequence, or None if the sequence is empty.
+    - Union[Any, None]: The first element of the sequence if it exists, otherwise None.
     """
     if lst:
         return lst[0]
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-module: contains the safely_get_value function
+This module contains the safely_get_value function that retrieves a value from
+a dictionary using a key, with an optional default value if the key is not present.
 """
 
 from typing import Any, Mapping, TypeVar, Union
@@ -9,17 +10,16 @@ T = TypeVar('T')
 
 def safely_get_value(dct: Mapping[Any, Any], key: Any, default: Union[T, None] = None) -> Union[Any, T]:
     """
-    Safely get a value from the dictionary dct 
-    If the key is not found, return the default value
+    Safely get a value from the dictionary dct using the provided key.
+    If the key is not found, return the default value.
 
-    Args:
-    	dct (Mapping[Any, Any]): The dictionary to search.
-    	key (Any): The key to look for in the dictionary.
-    	default (Union[T, None]): if the key is not found.
+    Parameters:
+    - dct (Mapping[Any, Any]): The dictionary to search.
+    - key (Any): The key to look for in the dictionary.
+    - default (Union[T, None]): The default value to return if the key is not found.
 
     Returns:
-       Union[Any, T]: The value from the dictionary if the
-       key is found, otherwise the default value.
+    - Union[Any, T]: The value from the dictionary if the key is found, otherwise the default value.
     """
     if key in dct:
         return dct[key]
