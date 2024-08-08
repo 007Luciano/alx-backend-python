a7c343ba94f35b693378b68b93e34c866226dc49
Luciano7 <lucky.h.fatch@gmail.com>
2024-08-08 11:10:10 +0000

    update runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,6 +9,7 @@ from importlib import import_module as using
 
 async_comprehension = using('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """
     Executes async_comprehension and measures the
